a1587eee1d2fdcc477d65af9899b07485bac4b4f
Martin8590 <afgritos@gmail.com>
2014-07-20 20:29:14 +0200

    Update Subject.cpp
    Kommentiert und soweit fertig.

--- Subject.cpp
+++ Subject.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <urlmon.h>
 #include <WinInet.h>
-#include <wininet.h>
-#include <qt_windows.h>
+#include <qmessagebox.h>
 using namespace std;
 
 void Subject::Attach(Observer *graph)
@@ -18,8 +17,9 @@ void Subject::Detach(Observer *graph)
 
 void Subject::Notify(string data)
 {
-    for(_iterator it = _observers.begin(); it != _observers.end(); ++it){
-            (*it)->Update(data);
+    for(_iterator it = _observers.begin(); it != _observers.end(); ++it)
+    {
+        (*it)->Update(data);
     }
 
 }
@@ -27,70 +27,99 @@ void Subject::Notify(string data)
 void Subject::Notify()
 {
 
-
-    for(_iterator it = _observers.begin(); it != _observers.end(); ++it){
-            (*it)->Update();
+    for(_iterator it = _observers.begin(); it != _observers.end(); ++it)
+    {
+        (*it)->Update();
     }
 
 }
 
-
+//Lädt Datei vom Server herunter
 void Subject::Download()
 {
-    //hr = URLDownloadToFile (0, Url, File, 0, 0);
+    QMessageBox DownloadFail;
+    QMessageBox DownloadGood;
+    DownloadFail.setText((QString)"Fehler beim Download");
+    DownloadGood.setText((QString)"Internetverbindung wieder Aktiv, download wieder möglich");
+
     DeleteUrlCacheEntry(TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"));
-    HRESULT hr = URLDownloadToFileW( NULL, TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"),TEXT("myfile.txt"), 0, NULL );
+    HRESULT hr = URLDownloadToFileW( NULL, TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"),TEXT("\\temp\\myfile.txt"), 0, NULL );
 
+    //Fängt Fehler ab, falls Internetverbindung abbricht oder nicht vorhanden ist
+    if(hr == S_OK)
+    {
+        if(InternetAktiv == false)
+        {
+            DownloadGood.exec();
+        }
+
+        InternetAktiv = true;
+    }
+    else
+    {
+        if(InternetAktiv == true)
+        {
+            DownloadFail.exec();
+        }
+
+        InternetAktiv = false;
+    }
     Parse();
-
 }
 
 
-
-
-
+//Ruft Notify auf und übergibt die Daten an die einzelnen Observer
 void Subject::Parse()
 {
-   ifstream dataStream ("C:\\Users\\Elvis\\Desktop\\build-SWT-Desktop_Qt_5_2_1_MinGW_32bit_qt_521-Debug\\myfile.txt");
 
-    while( dataStream)
-    {
-        getline(dataStream, line);
-        //if(line == "")
-         //   return;
-        temp = "";
-        tempOrt = "";
-        tempTemperatur = "";
-        if(line[0] != 35)
-        {
-            for( i=0; line[i] != ',' ; i++)
-            {
-                tempOrt = tempOrt + line[i];
-            }
-            Notify(tempOrt);
-            //cout << tempOrt << endl;
+   ifstream DataStream ("\\temp\\myfile.txt");
+   QMessageBox FileError;
+   FileError.setText((QString)"Datei nicht vorhanden");
 
-            for( i; i < line.size(); i++)
+   if(DataStream != 0) //Überpruft ob Datei vorhanden
+   {
+      while( DataStream)
+      {
+            getline(DataStream, Line);
+
+            Temp = "";
+            TempOrt = "";
+            TempTemperatur = "";
+
+            if(Line[0] != 35)//Überpruft ob Zeile am Anfang eine # ist
             {
-                if(line[i] != ',' && line[i] != ' ')
-                {
-                    tempTemperatur = tempTemperatur + line[i];
-                }
-            }
-            Notify(tempTemperatur);
-            //cout << tempTemperatur << endl;
+                 for( i=0; Line[i] != ',' ; i++)//Holt sich die Orte aus Datei
+                 {
+                     TempOrt = TempOrt + Line[i];
+                 }
+
+                 Notify(TempOrt);
+
+                 for( i; i < Line.size(); i++)//Holt sich die Temperaturen aus der Datei
+                 {
+                     if(Line[i] != ',' && Line[i] != ' ')
+                     {
+                         TempTemperatur = TempTemperatur + Line[i];
+                     }
+                 }
+                 Notify(TempTemperatur);
+             }
+             else
+             {
+                 for( i=0; i < Line.size() ; i++)//Holt sich Metadaten aus Datei
+                 {
+                     Temp = Temp + Line[i];
+                 }
+                 Notify(Temp);
+             }
+
         }
-        else
-        {
-            for( i=0; i < line.size() ; i++)
-            {
-                temp = temp + line[i];
-            }
-            Notify(temp);
-            //cout << temp << endl;
-        }
+   }
+   else//Falls Datei nicht vorhanden wird ein Fehler ausgegeben
+   {
+       FileError.exec();
+   }
 
-    }
+   Notify();
 
-    Notify();
 }
